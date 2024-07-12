2f0f94d1508f57a20afd36e0735331f2ad263887
muhammad-mansur <muhammadmansur737@gmail.com>
2024-07-12 13:02:16 +0530

    update

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -10,3 +10,9 @@ class DBStorage:
     
     def __init__(self):
         """ create engine """
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}:3306/{}'
+                                      .format(getenv('HBNB_MYSQL_USER'),
+                                              getenv('HBNB_MYSQL_PWD'),
+                                              getenv('HBNB_MYSQL_HOST'),
+                                              getenv('HBNB_MSQL_DB')),
+                                      pool_pre_ping=True)
