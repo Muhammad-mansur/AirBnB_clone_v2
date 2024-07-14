b40fe0d5ad09b206d8d25df1da82da2234e9ecf0
muhammad mansur <Muhammadmansur737@gmail.com>
2024-07-14 11:09:56 -0700

    shellcheck

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 # This script sets up web servers for the deployment of web_static
 
-
 # Update and install Nginx if it is not already installed
-if ! which nginx > /dev/null 2>&1; then
+if ! command -v nginx > /dev/null 2>&1; then
     sudo apt-get update -y
     sudo apt-get install -y nginx
 fi
@@ -19,13 +18,10 @@ echo "<html>
   <body>
     Holberton School
   </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html
+</html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
 
 # Create a symbolic link, recreating it if it already exists
-if [ -L /data/web_static/current ]; then
-    sudo rm /data/web_static/current
-fi
-sudo ln -s /data/web_static/releases/test/ /data/web_static/current
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 # Give ownership of /data/ folder to ubuntu user and group recursively
 sudo chown -R ubuntu:ubuntu /data/
