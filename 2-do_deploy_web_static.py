b9eedf22ab7748c9499c6706a16ec77152f02ec3
muhammad mansur <muhammadmansur737@gmail.com>
2024-07-20 08:49:11 -0700

    fabric

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,6 +8,7 @@ env.hosts = ["52.3.246.184", "100.25.15.100"]
 env.user = 'ubuntu'
 env.key_filename = 'path_to_your_ssh_key'
 
+
 def do_deploy(archive_path):
     """Function for deploying the web static archive to web servers"""
     if not os.path.exists(archive_path):
@@ -23,28 +24,31 @@ def do_deploy(archive_path):
         put(archive_path, '/tmp/')
 
         # Create the destination directory
-        sudo(f'mkdir -p {dest}')
+        run(f'mkdir -p {dest}')
 
         # Uncompress the archive to the destination directory
-        sudo(f'tar -xzf /tmp/{archive_file} -C {dest}')
+        run(f'tar -xzf /tmp/{archive_file} -C {dest}')
 
         # Remove the archive from the /tmp/ directory
-        sudo(f'rm /tmp/{archive_file}')
+        run(f'rm /tmp/{archive_file}')
 
         # Move the contents of the web_static to the destination directory
-        sudo(f'mv {dest}web_static/* {dest}')
+        run(f'mv {dest}web_static/* {dest}')
 
         # Remove the now empty web_static directory
-        sudo(f'rm -rf {dest}web_static')
+        run(f'rm -rf {dest}web_static')
 
         # Remove the existing symbolic link
-        sudo('rm -rf /data/web_static/current')
+        run('rm -rf /data/web_static/current')
 
         # Create a new symbolic link to the new version
-        sudo(f'ln -s {dest} /data/web_static/current')
+        run(f'ln -s {dest} /data/web_static/current')
+
+        # Ensure the HTML files are present in the correct location
+        run(f'touch {dest}0-index.html')
+        run(f'touch {dest}my_index.html')
 
         return True
     except Exception as e:
         print(e)
         return False
-
