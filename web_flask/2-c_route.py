d1b7b696487bb449479e934338068b69c8e7e3ad
muhammad-mansur <muhammadmansur737@gmail.com>
2024-08-06 21:27:30 +0100

    3

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -17,7 +17,9 @@ def hbnb():
     return "HBNB"
 
 @app.route("/c/<text>", strict_slashes=False)
-def c_is_fun():
+def c_is_fun(text):
+    """ Function that returns C is fun """
+    return "C {}".format(text.replace("_", " "))
 
 
 if __name__ == "__main":
