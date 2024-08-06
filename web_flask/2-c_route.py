bccb67f084022a5ecd9492722bb6494e26d2ab9b
muhammad-mansur <muhammadmansur737@gmail.com>
2024-08-06 21:29:03 +0100

    3

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -6,17 +6,17 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def hello_hbnb():
     """ Function that prints Hello HBNB """
     return "Hello HBNB!"
 
-@app.route("/hbnb", strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """ Function that returns HBNB """
     return "HBNB"
 
-@app.route("/c/<text>", strict_slashes=False)
+@app.route('/c/<text>', strict_slashes=False)
 def c_is_fun(text):
     """ Function that returns C is fun """
     return "C {}".format(text.replace("_", " "))
