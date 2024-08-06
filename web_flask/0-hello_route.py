f1348b26cabb6d755752abad0eb053c93fa5ff91
muhammad-mansur <muhammadmansur737@gmail.com>
2024-08-06 21:09:56 +0100

    hello flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """ Function that prints 'Hello HBNB' """
+    """ Function that prints Hello HBNB """
     return "Hello HBNB!"
 
 
