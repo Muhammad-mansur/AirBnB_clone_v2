187db6f8e2bdb1f9f315799398148ef874ebadd1
muhammad-mansur <muhammadmansur737@gmail.com>
2024-08-08 17:27:12 +0100

    3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" 3. Python is cool! """
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ Function that returns Hello HBNB """
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Function that returns HBNB """
+    return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """ Function that returns C is fun """
+    return "c {}".format(text.replace("_", " "))
+
+@app.route('/python/<text>', strict_slashes=False)
+def python_is_cool(text):
+    """ Function that returns Python """
+    return "Python {}".format(text.replace("_", " "))
+
+
+if __name__ == "__main":
+    app.run(host="0.0.0.0", port=5000)
